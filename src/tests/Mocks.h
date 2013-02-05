dddfaf332913e9823a6a6752f2691f5b4d0dc417
EvgenySafronov <division494@gmail.com>
2013-02-05 12:30:57 +0400

    Добавлены mock-объекты для SharedMemory и BeanFactory классов

--- src/tests/Mocks.h
+++ src/tests/Mocks.h
@@ -9,3 +9,35 @@ public:
     MOCK_CONST_METHOD3(push, void(TimeStamp, const SignalValue*, const void*));
     MOCK_CONST_METHOD1(getBuffersDump, char*(const void*));
 };
+
+class SharedMemoryMock : public SharedMemory {
+public:
+    MOCK_METHOD2(create, bool(int, AccessMode));
+    MOCK_CONST_METHOD0(size, int());
+    MOCK_METHOD1(attach, bool(AccessMode));
+    MOCK_CONST_METHOD0(isAttached, bool());
+
+    MOCK_CONST_METHOD0(constData, void*());
+    MOCK_METHOD0(data, void*());
+
+    MOCK_METHOD1(setKey, void(const QString&));
+    MOCK_CONST_METHOD0(getKey, QString());
+
+    MOCK_METHOD0(lock, bool());
+    MOCK_METHOD0(unlock, bool());
+
+    MOCK_CONST_METHOD0(getErrorDescription, QString());
+};
+
+class BeanFactoryMock : public BeanFactory {
+public:
+    SharedMemory *createSharedMemoryBean() const {
+        return new SharedMemoryMock;
+    }
+
+    LowLevelBufferHandler *createLowLevelBufferHandler(BufferId buffersCount, BufferPos bufferSize) const {
+        Q_UNUSED(buffersCount);
+        Q_UNUSED(bufferSize);
+        return new LowLevelBufferHandlerMock;
+    }
+};
