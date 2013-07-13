c66152bc6ab3af35b7f83c0120f73fd7af3a86fe
Evgeny Safronov <division494@gmail.com>
2013-07-13 16:59:00 +0400

    Заготовки для хранения метаинформации

--- src/lib/domain/MetaData.h
+++ src/lib/domain/MetaData.h
@@ -3,21 +3,24 @@
 #include "SharedBufferGlobal.h"
 #include <cstring>
 
+struct MetaDataHeader {
+    BufferPos currentPos = 0;
+    quint32 clientCount = 0;
+    BufferPos bufferSize = 0;
+    BufferId buffersCount = 0;
+};
+
 template<uint placeholderSize>
-struct MetaDataStruct {
-    BufferPos currentPos;
+struct MetaDataStruct : public MetaDataHeader {
     char placeholder[placeholderSize];
 
-    MetaDataStruct() : currentPos(0) {
+    MetaDataStruct() {
         std::memset(placeholder, 0, placeholderSize);
     }
 };
 
 template<>
-struct MetaDataStruct<0> {
-    BufferPos currentPos;
-    MetaDataStruct() : currentPos(0) {}
-};
+struct MetaDataStruct<0> : public MetaDataHeader {};
 
-struct MetaData : MetaDataStruct<124> {};
+struct MetaData : MetaDataStruct<112> {};
 static_assert(sizeof(MetaData) == 128, "sizeof(MetaData) must be equal 128 for language frameworks compatibility");
